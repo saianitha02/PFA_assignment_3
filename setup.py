9508e92747e26835cf2ac753fec2c0740691a4cc
saianitha02 <113101887+saianitha02@users.noreply.github.com>
2022-12-07 20:48:32 +0530

    Delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-setup(name='Package',
-      version='1.0',
-      description='Python functions',
-      author='SaiAnitha',
-      author_email='h20220847 @pilani.bits-pilani.ac.in',
-      packages=['saianitha'],
-     )
